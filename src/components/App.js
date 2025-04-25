6a412d4178617e8d01972b6e9b9c6799280d77c7
Parikshit <Parikshitmali17@gmail.com>
2025-04-25 19:24:56 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [NoClick,setNoClicl]=useState(0);
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button Clicked {NoClick} times</p>
+   <button onClick={()=>setNoClicl(NoClick+1)}>Click Me</button>
+        
     </div>
   )
 }
